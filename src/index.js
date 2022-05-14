2b2abe15ff11073c660e0fb4ed666017dab4b24e
sunday stanley Idung <sundaystanley56@gmail.com>
2022-05-14 13:23:05 +0100

    Add styling to my page

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
-import './style.css';
-import UI from './modules/UI.js';
-import Newscore from './modules/newScore.js';
-import { createGame, pushScore, fetchScore } from './modules/call-Api.js';
+import './style.css'
+import UI from './modules/UI.js'
+import Newscore from './modules/newScore.js'
+import { createGame, pushScore, fetchScore } from './modules/call-Api.js'
 
 const load = async () => {
-  const formInput = document.getElementById('form');
-  const refresh = document.querySelector('.btn');
+  const formInput = document.getElementById('form')
+  const refresh = document.querySelector('.btn')
 
   refresh.addEventListener('click', async () => {
-    await fetchScore();
-  });
+    await fetchScore()
+  })
 
   formInput.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const nameInput = document.querySelector('#input').value;
-    const scoreInput = document.querySelector('#number').value;
-    const scoreClass = new Newscore(nameInput, scoreInput);
-    await pushScore(scoreClass);
-    await UI.clearField();
-  });
-  await createGame();
-  await fetchScore();
-};
+    const pop = document.querySelector(".pop")
+    event.preventDefault()
 
-load();
+    if (!event.target.elements.name.value || !event.target.elements.score.value) {
+      pop.classList.remove('hide-bg')
+      pop.classList.add('active-bg')
+      pop.textContent = 'Please, Fill All Empty Field.'
+      setTimeout(() => {
+        pop.classList.add('hide-bg')
+        pop.classList.remove('active-bg')
+        pop.textContent = ''
+      }, 3000)
+      return
+    }
+    const nameInput = document.querySelector('#input').value
+    const scoreInput = document.querySelector('#number').value
+    const scoreClass = new Newscore(nameInput, scoreInput)
+    await pushScore(scoreClass)
+    await UI.clearField()
+    pop.classList.remove("hide-bg")
+    pop.classList.add("active-bg");
+    pop.textContent = 'Your score was added, Please hit the refresh button to see the list.'
+
+    setTimeout(() => {
+     pop.classList.add('hide-bg')
+     pop.classList.remove('active-bg')
+      pop.textContent = ''
+    }, 3000)
+  })
+  await createGame()
+  await fetchScore()
+}
+
+load()
